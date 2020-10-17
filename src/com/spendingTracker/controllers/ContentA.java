b6d1d711a71c8e62e1c5916b39506b0eb697d8da
java_learning <patrikhajnal93@gmail.com>
2020-10-17 15:54:24 +0200

    Removed unused imports.

--- src/com/spendingTracker/controllers/ContentA.java
+++ src/com/spendingTracker/controllers/ContentA.java
@@ -6,16 +6,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
+
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Box;
 
 import java.io.IOException;
-import java.util.Date;
+
 
 public class ContentA extends AnchorPane {
 
@@ -31,7 +29,7 @@ public class ContentA extends AnchorPane {
         this.data = data;
     }
 
-    public ContentA(){
+    public ContentA() {
         this.data = FXCollections.observableArrayList();
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/spendingTracker/fxml/ContentA.fxml"));
@@ -41,24 +39,24 @@ public class ContentA extends AnchorPane {
 
         try {
             fxmlLoader.load();
-            TableColumn<Spendings,String> dateCol = new TableColumn<>("Date");
+            TableColumn<Spendings, String> dateCol = new TableColumn<>("Date");
             dateCol.setCellValueFactory(new PropertyValueFactory<>("Date"));
-            TableColumn<Spendings,String> amountCol = new TableColumn<>("Amount");
+            TableColumn<Spendings, String> amountCol = new TableColumn<>("Amount");
             amountCol.setCellValueFactory(new PropertyValueFactory<>("Amount"));
-            TableColumn<Spendings,String> categoryCol = new TableColumn<>("Category");
+            TableColumn<Spendings, String> categoryCol = new TableColumn<>("Category");
             categoryCol.setCellValueFactory(new PropertyValueFactory<>("Category"));
 
 
-            data.add(new Spendings("2020-10-17", "100","Fueling"));
-            data.add(new Spendings("2020-10-17", "200","Grocery"));
-            data.add(new Spendings("2020-10-17", "300","Fueling"));
-            data.add(new Spendings("2020-10-17", "400","Fueling"));
+            data.add(new Spendings("2020-10-17", "100", "Fueling"));
+            data.add(new Spendings("2020-10-17", "200", "Grocery"));
+            data.add(new Spendings("2020-10-17", "300", "Fueling"));
+            data.add(new Spendings("2020-10-17", "400", "Fueling"));
 
             dateCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDate()));
             amountCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAmount()));
             categoryCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCategory()));
             tblTransactions.setItems(data);
-            tblTransactions.getColumns().addAll(dateCol,amountCol,categoryCol);
+            tblTransactions.getColumns().addAll(dateCol, amountCol, categoryCol);
         } catch (IOException e) {
             e.printStackTrace();
         }
